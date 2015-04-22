bf71d4777adedf0e8a2f9803b7d86d710ca568f4
megraff <meganmrafferty@gmail.com>
2015-04-22 18:18:27 -0400

    Update users.php

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,22 +1,63 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Users extends CI_Controller {
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('User');
+  }
   public function login()
   {
-    // load the user model
-    // check if email exists
-    // check if passwords match
-    // if it matches go to new post/posts
-    $this->load->library("form_validation");
-    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[persons.email]');
-    $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|matches[confirm_password]');
-    $this->form_validation->set_rules('confirm_password', 'Password Confirmation', 'required');
-    $this->load->view('users/login');
+    $email = $this->input->post('email');
+    $password =$this->input->post('password');
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('email','Email' , 'required');
+    $this->form_validation->set_rules('password', 'Password', 'required');
+    $user = $this->User->get_user_by_id($id);
+    if($user && $user['password'] == $password)
+    {
+      $current_user = array(
+        'user_id' => $user['id'],
+        'user_email' => $user['email'],
+        'user_name' => $user['user_name'],
+        'is_logged_in' => true
+        );
+      $data['user'] = $current_user;
+      $this->session_set_userdata($current_user);
+      $this->load->view('users/login');
+    }
+    else
+    {
+      $this->session->set_flashdata("login error", "Invalid email or password!")
+      redirect('posts/index');
+    }
+    
   }
-
   public function user_new()
   {
-    $this->load->view('users/user_new');
+    $user = $this->input->post();
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('user_name','Username' , 'required');
+    $this->form_validation->set_rules('email', 'Email', 'required');
+    $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[6]|matches[confirm_password]');
+    if($this->form_validation->run() == FALSE)
+    {
+      $errors = array(validation_errors());
+      $this->session->set_flashdata('errors', $errors);
+      redirect('/');
+    }
+    else
+    {
+      $this->User->register($user);
+      $current_user = array(
+        'user_name' => $user['user_name'],
+        'student_email' => $student['email']
+        );
+      $data['user'] = $current_user;
+      $this->load->view('users/user_new');
+    }
   }
 
   public function create()
